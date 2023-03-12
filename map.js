4c9bdee081155a852987f82a23762b06c2fa1649
Di <diana.miheenko.98@gmail.com>
2023-03-12 23:22:45 +0300

    Карта

---
+++ map.js
@@ -0,0 +1,23 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.93855426475575,30.323020794150363],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark([59.938654059375445,30.322849132773417], {
+
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'map.png',
+            iconImageSize: [113, 106],
+            iconImageOffset: [-25, -45]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+
+});
